c84341505d29bfcfb3d476a7ab7fad61c6a31c89
Chen <Chen@DESKTOP-LBABRGD>
2016-12-28 14:47:21 +0800

    contact

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,7 @@
 ﻿var express = require('express');
 var router = express.Router();
-var db = require('../db/db');
+//var db = require('../db/db');
+var dbHelper = require('../db/dbHelper');
 
 router.get('/info', function (req, res) {
     if (!req.query.accountnumber)
@@ -9,22 +10,26 @@ router.get('/info', function (req, res) {
     var query = 'select a.accountnumber customercode,c.name customername,c.accountid ' +
         '     , c.firstname,c.lastname, c.email, c.mobilephone ,u.Username salesrep,u.MobilePhone salesrepphone' +
         ' from contact  c ' +
-        ' inner join account a on c.accountid = a.sfid ' +
-        ' inner join user u on u.ebMobile__usercode__c = a.ebmobile__salesroute__c ' +
-        ' where ebmobile__primary__c= true and a.accountnumber = \'' + req.query.accountnumber + '\'';
-    db.query(query).then(function (result) {
-        res.json(result.rows);
-    }).catch(function (err) {
-        console.error(err);
+        ' inner join account a on c.accountid = a.id ' +
+        ' inner join [user] u on u.ebMobile__usercode__c = a.ebmobile__salesroute__c ' +
+        ' where ebmobile__primary__c= 1 and a.accountnumber = \'' + req.query.accountnumber + '\'';
+    dbHelper.query(query, function (err, result) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.json(result);
     });
 });
 
 router.post('/update', function (req, res) {
     var query = 'update contact set firstname=\'' + req.body.firstname + '\',lastname=\'' + req.body.lastname + '\',email=\'' + req.body.email + '\',mobilephone=\'' + req.body.mobile + '\' where accountid=\'' + req.body.accountid + '\'';
-    db.query(query).then(function (result) {
+    dbHelper.query(query, function (err, result) {
+        if (err) {
+            res.json({ err_code: 1, err_msg: 'insert failed:' + err.message });
+            return;
+        }
         res.json({ err_code: 0, err_msg: 'insert success!' });
-    }).catch(function (err) {
-        res.json({ err_code: 1, err_msg: 'insert failed:' + err.message });
     });
 });
 
